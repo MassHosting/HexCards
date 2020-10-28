f2a610e924f916a89a51a13211407e2ecf78c5a3
JBarti <josip.bartulovic3@skole.hr>
2020-10-28 22:07:06 +0100

    Added production api url

--- HexCards.9ad09f98.js
+++ HexCards.9ad09f98.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37155" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33169" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
